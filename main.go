f79d7550615cbf1e0bcdae54a85d46c49fac59e7
André Wronscki <andrewronscki@gmail.com>
2023-08-06 13:03:55 -0300

    como declarar variaveis

--- main.go
+++ main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	var variavel1 string = "Variavel 1"
+	variavel2 := "Variavel 2"
+	fmt.Println(variavel1)
+	fmt.Println(variavel2)
+
+	var (
+		variavel3 string = "variavel 3"
+		variavel4 string = "variavel 4"
+	)
+	fmt.Println(variavel3)
+	fmt.Println(variavel4)
+
+	variavel5, variavel6 := "Variavel 5", "Variavel 6"
+	fmt.Println(variavel5)
+	fmt.Println(variavel6)
+
+	const constante1 string = "Constante 1"
+	fmt.Println(constante1)
 }
